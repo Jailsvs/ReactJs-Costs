c3f909dd42164d422f589ab947a1b796c2564f66
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-01-12 19:56:06 -0300

    Criando Project form

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,17 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
   return (
-    <h1>NewProject</h1>
+    <div className={styles.newProjectContainer}>
+      <h1>Criar Projeto</h1>    
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <p>Formulário</p>
+      <ProjectForm>
+
+      </ProjectForm>
+    </div>
+    
   )
 }
 
