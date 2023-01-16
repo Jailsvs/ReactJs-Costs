0cc68c260df9c8d3e88cdca79f7caabf209a8101
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-01-16 19:26:29 -0300

    Json server e fetch categories

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,8 +5,13 @@ function Select({text, name, options, handleOnChange, value}){
     <div className={styles.formControl}>
       <label htmlFor={name}>{text}</label>
       <select name={name} id={name}>
-        <option disabled>Selecione uma opção</option>
-        
+        <option disabled selected>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} 
+                  key={option.id}>
+          {option.name}
+          </option>
+        ))}
       </select>
     </div>  
   )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,25 @@
+import {useEffect, useState} from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}){
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then((resp) => resp.json())
+          .then((data) => {
+            setCategories(data)
+          })
+          .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input  type='text' 
@@ -14,7 +30,10 @@ function ProjectForm({btnText}){
               text='Orçamento Total'
               placeholder='Insira o orçamento total'
               name='budget'/>
-      <Select name='category_id' text='Categoria'  />     
+      <Select name='category_id' 
+              text='Categoria' 
+              options={categories}
+      />     
       <SubmitButton text={btnText} />
     </form>
   )
