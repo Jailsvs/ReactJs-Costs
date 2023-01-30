b60c351282c6bde2e7ea7fb25643eeb1e683ad5f
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-01-30 07:43:10 -0300

    Edição de Project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,11 +3,13 @@ import styles from './Project.module.css'
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Loading from '../layout/Loading'
+import Container from '../layout/Container';
 
 function Project(){
 
   const {id} = useParams()
   const [project, setProject] = useState([])
+  const [showProjectForm, setShowProjectForm] = useState(false)
   const delayServer = 3000
 
   useEffect(() => {
@@ -25,9 +27,42 @@ function Project(){
     }, delayServer)
   }, [id])
 
+  function toogleProjectForm () {
+    setShowProjectForm(!showProjectForm)
+  }
+
   return (
     <>
-    {project.name ? (<p>{project.name}</p>) : (<Loading/>)}
+    {project.name ? 
+      (<div className={styles.projectDetails}>
+        <Container customClass="column">
+          <div className={styles.detailsContainer}>
+            <h1>Projeto: {project.name}</h1>
+            <button onClick={toogleProjectForm}
+                    className={styles.btn}>
+              {!showProjectForm ? "Editar projeto" : "Fechar"}
+            </button>
+            {!showProjectForm ? (
+              <div className={styles.projectInfo}>
+                <p>
+                  <span>Categoria:</span> {project.category.name}
+                </p>
+                <p>
+                  <span>Total Orçamento:</span> R${project.budget}
+                </p>
+                <p>
+                  <span>Total Utilizado:</span> R${project.cost}
+                </p>
+              </div>) 
+              : (
+                <div className={styles.projectInfo}>
+                  <p>detalhes projeto</p>
+                </div>) 
+            }
+          </div>
+        </Container>
+      </div>)
+    : (<Loading/>)}
     </>
   )
 }
